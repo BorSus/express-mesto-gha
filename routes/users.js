7b8207893125231678950b3aa462eb1ff625ffb2
Boris Sustavov <sbv91@mail.ru>
2023-07-23 02:07:13 +0300

    save before review

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,10 @@ const {
   patchUserInfo,
   patchUserAvatar
 } = require('../controllers/users');
-// GET /users/me
-router.get('/users/me', getCurrentUser);
 //  GET /users/:userId - возвращает пользователя по _id
 router.get('/users/:id', validatorSchemaGetUserById, getUserById);
+// GET /users/me
+router.get('/users/me', getCurrentUser);
 
 //  GET /users — возвращает всех пользователей
 router.get('/users', getAllUsers);
