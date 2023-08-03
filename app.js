09fbbebff520d33c81abf2bcc8d3c3b0189b7410
Boris Sustavov <sbv91@mail.ru>
2023-08-03 22:32:12 +0300

    save before uploading

--- app.js
+++ app.js
@@ -26,15 +26,17 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const NotFound = require('./utils/errors/not-found');
 
-const { PORT = 5000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 app.use(helmet());
+// подключаем CORS мидлвару
+//  app.use(corsMiddleware);
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
-      'https://mesto.sustavov.nomoreparties.co',
+      'http://localhost:3001',
+      'http://mesto.sustavov.nomoreparties.co',
       'https://mesto.sustavov.nomoreparties.co'
     ],
 
@@ -55,8 +57,7 @@ mongoose
 
 // подключаем логгер запросов
 app.use(requestLogger);
-// подключаем CORS мидлвару
-//app.use(corsMiddleware);
+
 //  Подключение путей авторизации
 //  POST /signup — создаёт пользователя
 app.post('/signup', validatorSchemaPostNewUser, postNewUser);
